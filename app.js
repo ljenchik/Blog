4eaad1c142afda69b831903a791ced3cbebad292
Olena Pelagenko <84686704+ljenchik@users.noreply.github.com>
2023-05-15 10:55:15 +0100

    Add mongodb, user can create and update posts

--- app.js
+++ app.js
@@ -4,6 +4,21 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb+srv://olena:1234@cluster0.h0n8umz.mongodb.net/Blog", {
+  useNewUrlParser: true,
+});
+
+// Schema for creating a blog entry
+const blogsSchema = new mongoose.Schema({
+  title: String,
+  body: String,
+});
+
+const Blog = mongoose.model("Blog", blogsSchema);
+
+const app = express();
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -12,24 +27,16 @@ const aboutContent =
 const contactContent =
   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-const app = express();
-let posts = [];
-
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// Get all blogs
 app.get("/", function (req, res) {
-  res.render("home", { home: homeStartingContent, posts: posts });
-});
-
-app.get("/about", function (req, res) {
-  res.render("about", { about: aboutContent });
-});
-
-app.get("/contact", function (req, res) {
-  res.render("contact", { contact: contactContent });
+  Blog.find({}).then(function (storedBlogs) {
+    res.render("home", { home: homeStartingContent, posts: storedBlogs });
+  });
 });
 
 // Compose a blog entry
@@ -43,70 +50,83 @@ app.post("/compose", function (req, res) {
     body: req.body.entryBody,
     id: new Date(),
   };
-  posts.push(post);
+
+  const blog = new Blog(post);
+
+  Blog.insertMany([blog]);
   res.redirect("/");
 });
 
 app.get("/posts/:post", function (req, res) {
   const parameter = _.lowerCase(req.params.post);
-  posts.forEach(function (post) {
-    const postTitle = _.lowerCase(post.title);
-    if (postTitle === parameter) {
-      res.render("post", {
-        postTitle: postTitle,
-        postBody: post.body,
-        id: post.id,
-      });
-    }
+
+  Blog.find({}).then(function (foundBlogs) {
+    foundBlogs.forEach(function (post) {
+      const postTitle = _.lowerCase(post.title);
+      if (postTitle === parameter) {
+        res.render("post", {
+          postTitle: postTitle,
+          postBody: post.body,
+          id: post.id,
+        });
+      }
+    });
   });
 });
 
 // Update a blog entry
 app.get("/posts/:post/update", function (req, res) {
   const parameter = _.lowerCase(req.params.post);
-  posts.forEach(function (post) {
-    const postTitle = _.lowerCase(post.title);
-    if (postTitle === parameter) {
-      res.render("update", {
-        postTitle: postTitle,
-        postBody: post.body,
-        id: post.id,
-      });
-    }
+
+  Blog.find({}).then(function (foundBlogs) {
+    foundBlogs.forEach(function (post) {
+      const postTitle = _.lowerCase(post.title);
+      if (postTitle === parameter) {
+        res.render("update", {
+          postTitle: postTitle,
+          postBody: post.body,
+          id: post.id,
+        });
+      }
+    });
   });
 });
 
 app.post("/posts/:post/update", function (req, res) {
   const parameter = _.lowerCase(req.params.post);
-  let updatedPost = {};
-  let index = 0;
-  posts.forEach(function (post) {
-    const postTitle = _.lowerCase(post.title);
-    if (postTitle === parameter) {
-      updatedPost = {
-        title: req.body.entryTitle,
-        body: req.body.entryBody,
-        id: post.id,
-      };
-
-      let obj = posts.find((item) => item.id === post.id);
-      index = posts.indexOf(obj);
+  
+  Blog.findOneAndUpdate(
+    { title: parameter },
+    {
+      title: req.body.entryTitle,
+      body: req.body.entryBody,
+      id: req.body._id,
     }
-  });
-
-  posts[index] = updatedPost;
+  ).then(function(post) {
+    console.log("Updated!");
+    console.log(post);
+  }
+  );
   res.redirect("/");
 });
 
-app.post("/posts/:post/delete", function (req, res) {
-  const parameter = _.lowerCase(req.params.post);
-  posts.forEach(function (post) {
-    const postTitle = _.lowerCase(post.title);
-    if (postTitle === parameter) {
-      posts = posts.filter((element) => element.id != post.id);
-    }
-  });
-  res.redirect("/");
+// app.post("/posts/:post/delete", function (req, res) {
+//   const parameter = _.lowerCase(req.params.post);
+//   posts.forEach(function (post) {
+//     const postTitle = _.lowerCase(post.title);
+//     if (postTitle === parameter) {
+//       posts = posts.filter((element) => element.id != post.id);
+//     }
+//   });
+//   res.redirect("/");
+// });
+
+app.get("/about", function (req, res) {
+  res.render("about", { about: aboutContent });
+});
+
+app.get("/contact", function (req, res) {
+  res.render("contact", { contact: contactContent });
 });
 
 app.listen(3000, function () {
